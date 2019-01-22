fab90033f96b632ee066451fd476cf3e03ec932b
Gerald Bryant <hakeem1106@gmail.com>
2019-01-22 21:47:08 +0000

    updating

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
+document.getElementsByTagName("button").innerHTML= "enter";
 class App extends Component {
   render() {
     return (
       <div className="App">
 
+        <input>
+        </input>
+
+        <button></button>
+
+
       </div>
     );
+
   }
+
 }
 
 export default App;
